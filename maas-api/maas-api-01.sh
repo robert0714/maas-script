b42feb1bd87081e5a793e6b6c3a012066288255e
Robert <robert0714@gmail.com>
2017-10-17 05:46:10 -0500

    Update maas-api-01.sh

--- maas-api/maas-api-01.sh
+++ maas-api/maas-api-01.sh
@@ -3,7 +3,7 @@ export  count=$( ps -aef|grep platform  -c)
 echo "value:  $count "
 if [[ $count > 1  ]]; then
    kill -9 $( ps -aef|grep platform  |head -1|awk '{print$2}' )  
-}
+
 
 nohup  /usr/bin/java -jar $PWD/develop/platform-napi/0.0.1-SNAPSHOT/platform-napi-0.0.1-SNAPSHOT.jar  0<&- &>   $PWD/api.log   &
 
