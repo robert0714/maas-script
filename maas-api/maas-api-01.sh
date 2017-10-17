cc8fc1b5d03bbe4dc5127fb9e7a17f3714635a09
Robert <robert0714@gmail.com>
2017-10-17 05:24:33 -0500

    Update maas-api-01.sh

--- maas-api/maas-api-01.sh
+++ maas-api/maas-api-01.sh
@@ -1,5 +1,9 @@
-sh " kill -9 $( ps -aef|grep platform  |head -1|awk '{print$2}' )  "
+count  ps -aef|grep platform  |head -1|awk '{print$2}' -c
 
-sh " /usr/bin/java -jar $PWD/develop/platform-napi/0.0.1-SNAPSHOT/platform-napi-0.0.1-SNAPSHOT.jar  >  $PWD/api.log  "
+echo $count
+
+kill -9 $( ps -aef|grep platform  |head -1|awk '{print$2}' )  
+
+nohup  /usr/bin/java -jar $PWD/develop/platform-napi/0.0.1-SNAPSHOT/platform-napi-0.0.1-SNAPSHOT.jar  0<&- &>   $PWD/api.log   &
 
 curl http://192.168.57.75:8080/api/  |jq '.' 
