1cdec9c3bf8256c6318b888d3be6c1410a9318b1
Robert <robert0714@gmail.com>
2017-10-18 21:53:23 -0500

    Update maas-api-01.sh

--- maas-api/maas-api-01.sh
+++ maas-api/maas-api-01.sh
@@ -5,7 +5,9 @@ if [[ $count > 1  ]]; then
    kill -9 $( ps -aef|grep platform  |head -1|awk '{print$2}' )  
 fi
 
-nohup  /usr/bin/java -jar $PWD/develop/platform-napi/0.0.1-SNAPSHOT/platform-napi-0.0.1-SNAPSHOT.jar  0<&- &>   $PWD/api.log   &
+#nohup  /usr/bin/java -jar $PWD/develop/platform-napi/0.0.1-SNAPSHOT/platform-napi-0.0.1-SNAPSHOT.jar  0<&- &>   $PWD/api.log   &
+
+nohup  java -jar /data/jenkins_slaves/vip-npp/workspace/maas-api-03//develop/platform-napi/0.0.1-SNAPSHOT/platform-napi-0.0.1-SNAPSHOT.jar  0<&- &>   $PWD/api.log   &
 
 ps -aef|grep platform
 
