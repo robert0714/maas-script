f5c96bcf6772b5a009f3d0cbeb0b3a89b33f0e27
Robert <robert0714@gmail.com>
2017-10-21 16:17:19 +0800

    Update maas-api-01.sh

--- maas-api/maas-api-01.sh
+++ maas-api/maas-api-01.sh
@@ -1,14 +1,14 @@
-export  count=$( ps -aef|grep platform  -c)
+export  count=$( ps -aef|grep maas   -c)
 
 echo "value:  $count "
 if [[ $count > 1  ]]; then
-   kill -9 $( ps -aef|grep platform  |head -1|awk '{print$2}' )  
+   kill -9 $( ps -aef|grep  maas   |head -1|awk '{print$2}' )  
 fi
 
 #nohup  /usr/bin/java -jar $PWD/develop/platform-napi/0.0.1-SNAPSHOT/platform-napi-0.0.1-SNAPSHOT.jar  0<&- &>   $PWD/api.log   &
 
-nohup  java -jar /data/jenkins_slaves/vip-npp/workspace/maas-api-03//develop/platform-napi/0.0.1-SNAPSHOT/platform-napi-0.0.1-SNAPSHOT.jar  0<&- &>   $PWD/api.log   &
+nohup  java -jar ~/ maas-api.jar  0<&- &>   ~/api.log   &
 
-ps -aef|grep platform
+ps -aef|grep maas
 
-curl http://192.168.57.75:8080/api/  |jq '.' 
+#curl http://192.168.57.75:8080/api/  |jq '.' 
